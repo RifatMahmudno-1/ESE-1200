d7c4e3bedbc695e30d8ea8b0623e61e0aee95642
Rifat Mahmud <rifatmahmudpc@gmail.com>
2025-09-15 12:52:14 +0600

    added cw of 08th Sep

--- data/classworks.js
+++ data/classworks.js
@@ -16,4 +16,31 @@
 // 	]
 // }
 
-export default []
+export default [
+	{
+		title_anchor: '2025-09-08',
+		title_text: '08th September, 2025',
+		items: [
+			{
+				title: 'Classwork-1',
+				views: [
+					{
+						title: '3D model',
+						link: '/objects/cw-01.glb',
+						type: 'model'
+					}
+				]
+			},
+			{
+				title: 'Classwork-2',
+				views: [
+					{
+						title: '3D model',
+						link: '/objects/cw-02.glb',
+						type: 'model'
+					}
+				]
+			}
+		]
+	}
+]
